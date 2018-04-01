648671f755a04214c492b4ecd956028b39d997f4
Uliana Semkiv <ulyankass3@gmail.com>
2018-04-01 23:53:58 +0300

    add hello world program

---
+++ test1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
